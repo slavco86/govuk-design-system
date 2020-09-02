f2f48d34f43e12b1a670024c73f4f1c0df4f96e0
Am4nzi <gvs.0027@gmail.com>
2020-09-02 11:29:49 +0100

    add input  component

---
+++ src/components/GovukInput.vue
@@ -0,0 +1,12 @@
+<template>
+    <input class="govuk-input" id="address-line-1">
+</template>
+
+<script>
+    export default {
+        name: "GovukInput",
+        props: {
+            headingText: String,
+        },
+    };
+</script>
