ffd946a6c29aeff912544bec1983cf81c7266ef9
Am4nzi <gvs.0027@gmail.com>
2020-09-02 11:33:53 +0100

    add label component

---
+++ src/components/GovukLabel.vue
@@ -0,0 +1,15 @@
+<template>
+    <label class="govuk-label">
+        {{ labelText }}
+    </label>
+</template>
+
+<script>
+    export default {
+        name: "GovukLabel",
+        props: {
+            labelText: String,
+        },
+    };
+</script>
+e
