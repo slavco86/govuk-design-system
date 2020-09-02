4b21f3f8bd925d10cc54854bb1a9cecfac305bda
Am4nzi <gvs.0027@gmail.com>
2020-09-02 11:24:22 +0100

    add fieldset legend component

---
+++ src/components/GovukFieldsetLegend.vue
@@ -0,0 +1,12 @@
+<template>
+  <legend class="govuk-fieldset__legend govuk-fieldset__legend--l"></legend>
+</template>
+
+<script>
+export default {
+  name: "GovukFieldsetLegend",
+  props: {
+    legendText: String,
+  },
+};
+</script>
