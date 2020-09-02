1a9ef8cee1efba31d3378847cfa25793e6db6771
Am4nzi <gvs.0027@gmail.com>
2020-09-02 13:41:58 +0100

    add dummy text to fieldset legend (to be replaced dynamically later)

--- src/components/GovukFieldsetLegend.vue
+++ src/components/GovukFieldsetLegend.vue
@@ -1,5 +1,9 @@
 <template>
-  <legend class="govuk-fieldset__legend govuk-fieldset__legend--l"></legend>
+  <legend class="govuk-fieldset__legend govuk-fieldset__legend--l">
+    <h1 class="govuk-fieldset__heading">
+      What is your name?
+    </h1>
+  </legend>
 </template>
 
 <script>
