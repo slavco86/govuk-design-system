a8dffee4fc8974079e2c79e401685dc28058a9fa
Am4nzi <gvs.0027@gmail.com>
2020-09-02 11:14:09 +0100

    add button component

---
+++ src/components/GovukButton.vue
@@ -0,0 +1,14 @@
+<template>
+    <button class="govuk-button">
+        {{ buttonText }}
+    </button>
+</template>
+
+<script>
+    export default {
+        name: 'GovukButton',
+        props: {
+            buttonText: String
+        }
+    }
+</script>
