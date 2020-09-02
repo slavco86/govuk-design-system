19686e994d7e4c8d31823d07d91cc8c0297c55dc
Am4nzi <gvs.0027@gmail.com>
2020-09-02 11:22:01 +0100

    add fieldset heading component

---
+++ src/components/GovukFieldsetHeading.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1 class="govuk-fieldset__heading"> {{ headingText }}</h1>
+</template>
+
+<script>
+    export default {
+        name: 'GovukFieldsetHeading',
+        props: {
+            headingText: String
+        }
+    }
+</script>
