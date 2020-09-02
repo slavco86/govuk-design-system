d8d395b8e233016e62baecb5ad55470b542fc2fd
Am4nzi <gvs.0027@gmail.com>
2020-09-02 13:44:08 +0100

    add button to questions componenet and add dummy text to button

--- src/views/Questions.vue
+++ src/views/Questions.vue
@@ -2,18 +2,21 @@
   <div>
     <GovukHeader />
     <GovukFieldset />
+    <GovukButton button-text="Continue" />
   </div>
 </template>
 
 <script>
 import GovukHeader from "../components/GovukHeader";
 import GovukFieldset from "../components/GovukFieldset";
+import GovukButton from "../components/GovukButton";
 
 export default {
   name: "Questions",
   components: {
     GovukHeader,
-    GovukFieldset
+    GovukFieldset,
+    GovukButton
   },
 };
 </script>
