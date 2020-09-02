4d0e2f04fd3e2b843db2596b2a049e73c1bbc3c8
Am4nzi <gvs.0027@gmail.com>
2020-09-02 20:52:37 +0100

    add back link component

---
+++ src/components/GovukBackLink.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="govuk-width-container">
+    <a href="#" class="govuk-back-link">Back</a>
+  </div>
+</template>
--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <GovukHeader />
+    <GovukBackLink />
     <router-view />
   </div>
 </template>
 
 <script>
 import GovukHeader from "../components/GovukHeader";
+import GovukBackLink from "../components/GovukBackLink";
 export default {
   name: "Layout",
   components: {
     GovukHeader,
+    GovukBackLink,
   },
 };
 </script>
