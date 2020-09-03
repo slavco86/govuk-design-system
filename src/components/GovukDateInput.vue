3dab905bff50b47b57d92b243f79749ea94ceadc
Am4nzi <gvs.0027@gmail.com>
2020-09-03 12:15:47 +0100

    Add date input component

---
+++ src/components/GovukDateInput.vue
@@ -0,0 +1,17 @@
+<template>
+  <input
+    class="govuk-input govuk-date-input__input govuk-input--width-2"
+    id="dob-day"
+    name="dob-day"
+    type="text"
+    autocomplete="bday-day"
+    pattern="[0-9]*"
+    inputmode="numeric"
+  />
+</template>
+
+<script>
+    export default {
+        name: "GovUKDateInput",
+    };
+</script>
--- src/components/GovukDateInputWrapper.vue
+++ src/components/GovukDateInputWrapper.vue
@@ -4,6 +4,6 @@
 
 <script>
 export default {
-  name: "GovUKDateInput",
+  name: "GovUKDateInputWrapper",
 };
 </script>
