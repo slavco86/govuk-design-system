d49314be59588514e2a3dad98636d443d3b0d9b6
Am4nzi <gvs.0027@gmail.com>
2020-09-03 14:15:07 +0100

    add radios componenet

---
+++ src/components/GovukRadios.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="govuk-radios govuk-radios--inline">
+    <div class="govuk-radios__item">
+      <input class="govuk-radios__input" type="radio" value="yes" />
+      <label class="govuk-label govuk-radios__label"> </label>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "GovukRadios",
+    };
+</script>
