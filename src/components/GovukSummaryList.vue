9c46cb72a186b7697bcd104693d5f55c0b23b401
Am4nzi <gvs.0027@gmail.com>
2020-09-03 10:59:59 +0100

    build summary list component by nesting child components inside of it

--- src/components/GovukSummaryList.vue
+++ src/components/GovukSummaryList.vue
@@ -1,9 +1,16 @@
 <template>
-  <dl class="govuk-summary-list"></dl>
+  <dl class="govuk-summary-list">
+    <GovukSummaryListRow />
+  </dl>
 </template>
 
 <script>
+import GovukSummaryListRow from "../components/GovukSummaryListRow";
+
 export default {
   name: "GovukSummaryList",
+  components: {
+    GovukSummaryListRow,
+  },
 };
 </script>
--- src/components/GovukSummaryListRow.vue
+++ src/components/GovukSummaryListRow.vue
@@ -1,10 +1,21 @@
 <template>
   <div class="govuk-summary-list__row">
+    <GovukSummaryListKey />
+    <GovukSummaryListValue />
+    <GovukSummaryListActions />
   </div>
 </template>
 
 <script>
+  import GovukSummaryListKey from "../components/GovukSummaryListKey";
+  import GovukSummaryListValue from "../components/GovukSummaryListValue";
+  import GovukSummaryListActions from "../components/GovukSummaryListActions";
 export default {
   name: "GovukSummaryListRow",
+  components: {
+    GovukSummaryListKey,
+    GovukSummaryListValue,
+    GovukSummaryListActions
+  },
 };
 </script>
