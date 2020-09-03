a889d69d475c80d183886d79eb775f13da577b4c
Am4nzi <gvs.0027@gmail.com>
2020-09-03 10:36:04 +0100

    add summary list actions component

---
+++ src/components/GovukSummarListActions.vue
@@ -0,0 +1,13 @@
+<template>
+  <dd class="govuk-summary-list__actions">
+    <a class="govuk-link" href="#">
+      Change<span class="govuk-visually-hidden"> contact information</span>
+    </a>
+  </dd>
+</template>
+
+<script>
+    export default {
+        name: "GovukSummaryListActions",
+    };
+</script>
