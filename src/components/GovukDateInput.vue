17ac9683045b20eba047f58a4bc76b62d51932a6
Am4nzi <gvs.0027@gmail.com>
2020-09-04 12:37:23 +0100

    limit character count in date of birth inputs

--- src/components/GovukDateInput.vue
+++ src/components/GovukDateInput.vue
@@ -18,6 +18,7 @@
             autocomplete="bday-day"
             pattern="[0-9]*"
             inputmode="numeric"
+            maxlength="2"
           />
         </div>
       </div>
@@ -35,6 +36,7 @@
             autocomplete="bday-month"
             pattern="[0-9]*"
             inputmode="numeric"
+            maxlength="2"
           />
         </div>
       </div>
@@ -52,6 +54,7 @@
             autocomplete="bday-year"
             pattern="[0-9]*"
             inputmode="numeric"
+            maxlength="4"
           />
         </div>
       </div>
