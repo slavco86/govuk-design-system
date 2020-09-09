11b151c4d2ea928209a3522066a1c86bdb7f64d9
SlavCo Zute <slavick.slavick@gmail.com>
2020-09-09 18:07:01 +0100

    Update GovukRadios.vue

--- src/components/GovukRadios.vue
+++ src/components/GovukRadios.vue
@@ -1,24 +1,37 @@
 <template>
-  <div>
-    <div class="govuk-radios__item">
-      <input
-        v-model="inputValue"
-        class="govuk-radios__input"
-        type="radio"
-        :value="formSelectorOptions"
-      />
-      <label class="govuk-label govuk-radios__label">
-        {{ formSelectorOptions }}
-      </label>
-    </div>
+  <div class="govuk-radios__item">
+    <input
+      class="govuk-radios__input"
+      type="radio"
+      :value="inputValue"
+      @change="$emit('input', $event.target.value)"
+      v-on="listeners"
+      v-bind="$attrs"
+    />
+    <label class="govuk-label govuk-radios__label" :for="$attrs.id">
+      <slot name="label">{{ label }}</slot>
+    </label>
   </div>
 </template>
-
 <script>
 export default {
-  name: "GovukRadios",
+  name: "GovukRadioInput",
+  inheritAttrs: false,
   props: {
-    formSelectorOptions: String,
+    inputValue: {
+      type: String,
+      required: true
+    },
+    label: {
+      type: String,
+      required: false
+    }
   },
+  computed: {
+    listeners() {
+      const { change, input, ...listeners } = this.$listeners;
+      return listeners;
+    }
+  }
 };
 </script>
